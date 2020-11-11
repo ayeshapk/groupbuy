54ed704093a1041647d86876fd95f20389a27aa0
AYESHA <sarunjaik@hotmail.com>
2020-11-11 16:39:12 +0700

    fix merchant visual

--- src/component/MerchantName.tsx
+++ src/component/MerchantName.tsx
@@ -23,12 +23,12 @@ const MerchantName: React.FunctionComponent<appProps> = ({
             {type === 'Paper' && <Paper style={{ margin: '10px',/*paddingTop:'2vh',paddingBottom:'2vh'*/ }}>
                 <Grid container spacing={0} direction="row" justify="space-around" alignItems="center" >
                     <Grid item xs={1} />
-                    <Grid item xs={4} style={{ padding: '15px', margin: '0 auto' }}>
+                    <Grid item xs={5} style={{ padding: '15px', margin: '0 auto' }}>
                             <AspectRatio ratio="1/1" >
-                                <img src={image} alt={imageAlt} style={{ borderRadius: '50%' }} />
+                                <img src={image} alt={imageAlt}  />
                             </AspectRatio>
                     </Grid>
-                    <Grid item xs={7} style={{ padding: '15px', margin: '0 auto' }}>
+                    <Grid item xs={6} style={{ padding: '15px', margin: '0 auto' }}>
                         <Typography variant='caption'>Merchant</Typography>
                         <Typography variant='h6'>{merchantName}</Typography>
                     </Grid>
@@ -37,12 +37,12 @@ const MerchantName: React.FunctionComponent<appProps> = ({
             {type === 'div' && <div style={{ margin: '10px',/*paddingTop:'2vh',paddingBottom:'2vh'*/ }}>
                 <Grid container spacing={0} direction="row" justify="space-around" alignItems="center" >
                     <Grid item xs={1} />
-                    <Grid item xs={4} style={{ padding: '15px', margin: '0 auto' }}>
+                    <Grid item xs={5} style={{ padding: '15px', margin: '0 auto' }}>
                             <AspectRatio ratio="1/1" >
-                                <img src={image} alt={imageAlt} style={{ borderRadius: '50%' }} />
+                                <img src={image} alt={imageAlt} />
                             </AspectRatio>
                     </Grid>
-                    <Grid item xs={7} style={{ padding: '15px', margin: '0 auto' }}>
+                    <Grid item xs={6} style={{ padding: '15px', margin: '0 auto' }}>
                         <Typography variant='caption'>Merchant</Typography>
                         <Typography variant='h6'>{merchantName}</Typography>
                     </Grid>
